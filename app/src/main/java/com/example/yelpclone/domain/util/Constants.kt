ad2374f862b4e5cc3ca069cfd161065f2f1c46a9
Stefan Bayne <stefanbusiness95@gmail.com>
2023-04-23 11:23:05 -0400

    updated jdk.

--- app/src/main/java/com/example/yelpclone/domain/util/Constants.kt
+++ app/src/main/java/com/example/yelpclone/domain/util/Constants.kt
@@ -2,5 +2,5 @@ package com.example.yelpclone.domain.util
 
 object Constants {
     const val API_KEY = "I3cSd58DqIsU6yoGD5oK_dePn3pejYBsyB312viwfWEd9Bm-VZ91tcwsevRUr_iJaXLxf3mCd6qg2TzYwGD3YzkBjOy_c0OPote7n-71nMb3Ukr3ETLGNyFdXTtFZHYx"
-    const val BASE_URL = "https://api.yelp.com/v3"
+    const val BASE_URL = "https://api.yelp.com/v3/"
 }
