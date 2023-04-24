fc76e563c99c76c64b111bed1254471c5d124a2b
Stefan Bayne <stefanbusiness95@gmail.com>
2023-04-23 22:26:35 -0400

    Splash screen and drawables.

--- app/src/main/java/com/example/yelpclone/presentation/view/ScreenActivity.kt
+++ app/src/main/java/com/example/yelpclone/presentation/view/ScreenActivity.kt
@@ -1,12 +1,46 @@
 package com.example.yelpclone.presentation.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.core.view.ViewCompat.animate
 import com.example.yelpclone.R
+import com.example.yelpclone.databinding.ActivitySplashScreenBinding
 
 class ScreenActivity : AppCompatActivity() {
+    private var _binding: ActivitySplashScreenBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
-    }
+        _binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.ivYelp.animate().apply {
+            duration = 300L
+            rotationXBy(180F)
+            rotationYBy(180F)
+        }.withEndAction {
+            binding.ivYelp.animate().apply {
+                duration = 500L
+                rotationXBy(-180F)
+                rotationYBy(-180F)
+            }
+        }
+
+        // delays before moving to other activity
+    Handler(Looper.getMainLooper()).postDelayed(
+    {
+        startActivity(
+            Intent(this@ScreenActivity, MainActivity::class.java)
+        )
+        finish()
+    }, 2000)
+}
+
+override fun onDestroy() {
+    super.onDestroy()
+    _binding = null
+}
 }
