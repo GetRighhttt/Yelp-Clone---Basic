ecdd4ac931cee9a7102b13a9155242ed451f8379
Stefan Bayne <stefanbusiness95@gmail.com>
2023-04-27 18:59:34 -0400

    commenting out paging code.

--- app/src/main/java/com/example/yelpclone/data/api/PagingSource.kt
+++ app/src/main/java/com/example/yelpclone/data/api/PagingSource.kt
@@ -4,16 +4,8 @@ package com.example.yelpclone.data.api
 The Yelp api does not provide a paging parameter that we can read in. They also have a limit of
 only 50 total results, so for this example, we will not use the Paging3 library.
  */
-class PagingSource{
-//    private val yelpService: YelpService,
-//    private val query: String
-//    ) : PagingSource<Int, YelpRestaurants>() {
-//
-//    override fun getRefreshKey(state: PagingState<Int, YelpRestaurants>): Int? {
-//        TODO("Not yet implemented")
-//    }
-//
-//    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, YelpRestaurants> {
-//        TODO("Not yet implemented")
-//    }
-}
+//class PagingSource(
+//    private val query: String,
+//    private val userService: UserService
+//    ) : PagingSource<>() {
+//}
