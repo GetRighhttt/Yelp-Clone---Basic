29c3a423e0244d5838ea4ee62b80daff5dfb8dbd
Stefan Bayne <stefanbusiness95@gmail.com>
2023-05-31 10:47:16 -0400

    refactoring and removing unnecessary database code.

--- app/src/main/java/com/example/yelpclone/domain/sot/YelpRepository.kt
+++ app/src/main/java/com/example/yelpclone/domain/sot/YelpRepository.kt
@@ -4,12 +4,6 @@ import com.example.yelpclone.core.events.Resource
 import com.example.yelpclone.data.model.yelp.YelpBusinesses
 import com.example.yelpclone.data.model.yelp.YelpSearchResult
 
-/*
-Acts as a single source of truth and additional layer of abstraction.
-Isolates the data layer from the rest of the application.
-The methods defined here wil be implemented in our repositoryImpl, which will
-then be used as a constructor dependency in our view model.
- */
 interface YelpRepository {
     suspend fun searchBusinesses(
         authHeader: String,
